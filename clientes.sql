041a0162fda8ae31aa44e7c9200571b94d65f6a1
Juank <eljuan95@hotmail.com>
2016-04-19 01:48:51 -1100

    Creacion de llaves primarias y foraneas
    Se creo la table tipoid y enlazo llaves primarias y foraneas.

--- clientes.sql
+++ clientes.sql
@@ -1,13 +1,22 @@
 CREATE DATABASE conalmi;
 SHOW DATABASES;
 USE conalmi;
+
+CREATE TABLE tipoid(
+tipoid VARCHAR(3),
+nombreid VARCHAR(40),
+PRIMARY KEY (tipoid));
+
 CREATE TABLE clientes(
 id BIGINT,
 tipoid VARCHAR(3),
 nombre VARCHAR(50),
-direccion VARCHAR(30),
+direccion VARCHAR(50),
 telefono INT,
 celular BIGINT,
-email VARCHAR(30)); 
-SHOW TABLES;
+email VARCHAR(50),
+PRIMARY KEY(id,tipoid),
+FOREIGN KEY(tipoid) REFERENCES tipoid(tipoid)); 
+
 DESCRIBE clientes;
+DESCRIBE tipoid;
